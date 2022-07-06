83757d8cf68f50e58507e999e58756e55e39bf35
cantuc40 <cantuc40@gator.uhd.edu>
2022-07-06 11:26:20 -0500

    imported SmsModule in app.module.ts

--- apps/api/messaging/src/app/app.module.ts
+++ apps/api/messaging/src/app/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SmsModule } from '../modules/sms/sms.module';
 
 @Module({
-  imports: [],
+  imports: [SmsModule],
   controllers: [AppController],
   providers: [AppService],
 })
