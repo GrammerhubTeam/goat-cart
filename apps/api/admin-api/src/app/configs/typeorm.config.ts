14b0b1bca1cb856a2760c787f2f96f8f13e7cd72
alexvcs <alexcurtisslep@gmail.com>
2022-07-18 16:03:14 -0400

    added dot env, configuring nest, and adding postgres configuration

---
+++ apps/api/admin-api/src/app/configs/typeorm.config.ts
@@ -0,0 +1,18 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
+
+* @param config (ConfigService)
+
+export const defaultConnection = {config: ConfigService}: TypeOrmModuleOptions & { seeds: string } => ({
+    type: 'postgres',
+    host:  config.get('TYPEORM_HOST'),
+    port: config.get('TYPEORM_PORT'),
+    username: config.get('TYPEORM_USERNAME'),
+    password: config.get('TYPEORM_PASSWORD'),
+    database: config.get('TYPEORM_DATABASE'),
+    autoLoadEntities: config.get('TYPEORM_AUTOLOAD'),
+    synchronize: config.get('TYPEORM_SYNCHRONIZE') == 'true',
+    logging: config.get('TYPEORM_LOGGING') == 'true',
+    entities: [config.get('TYPEORM_ENTITIES')], //needed for typeorm-seeding
+    seeds:  config.get('TYPEORM_SEEDING_SEEDS') //needed for typeorm-seeding
+});
