2ebcade8fc6fcbf4e1e1903f55d4f848774d23b9
alexvcs <alexcurtisslep@gmail.com>
2022-07-25 08:36:09 -0400

    added nest 9, working to resolve merge conflict

--- apps/api/admin-api/src/main.ts
+++ apps/api/admin-api/src/main.ts
@@ -4,15 +4,15 @@
  */
 
 import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3333;
+  const configService = app.get(ConfigService);
+  const port = configService.get('PORT');
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
