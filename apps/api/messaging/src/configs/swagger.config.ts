c77720919d0c6c73fe1cc6601572d5c3bf880e8c
Chris Cantu <44282951+cantuc40@users.noreply.github.com>
2022-07-25 22:38:57 +0000

    GC-58: Add Swagger Config (#5)
    * swagger config file and .gitkeep created
    
    * imported initSwagger function into main.ts
    
    * no significant changes
    
    * nestjs updated to version 9; swagger installed

---
+++ apps/api/messaging/src/configs/swagger.config.ts
@@ -0,0 +1,22 @@
+import type { INestApplication } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+export function initSwagger(app: INestApplication, port: number, prefix: string): string {
+	const version = '1.0';
+	const options = new DocumentBuilder()
+		.setTitle('Messaging API')
+		.setDescription('API Documentation for the Messaging app')
+		.setVersion(version)
+		.addTag('SMS, Email')
+		.addBearerAuth()
+		.build();
+
+	const document = SwaggerModule.createDocument(app, options);
+	SwaggerModule.setup(prefix + '/docs', app, document, {
+		swaggerOptions: {
+			persistAuthorization: true,
+		},
+	});
+
+	return `Documentation: http://localhost:${port}/${prefix}/docs`;
+}
--- apps/api/messaging/src/main.ts
+++ apps/api/messaging/src/main.ts
@@ -2,21 +2,41 @@
  * This is not a production server yet!
  * This is only a minimal backend to get started.
  */
-
-import { Logger } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
-
+import { ClassSerializerInterceptor, Logger, ValidationPipe } from '@nestjs/common';
+import { NestFactory , Reflector } from '@nestjs/core';
+import { ExpressAdapter, NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app/app.module';
+import { initSwagger } from './configs/swagger.config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+
+
+  const app = await NestFactory.create<NestExpressApplication>(
+    AppModule,
+    new ExpressAdapter(),
+    {cors: true }
+    );
+
   const globalPrefix = 'api';
+  const reflector = app.get(Reflector);
+  const port = process.env.PORT || 4000;
+
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3333;
-  await app.listen(port);
+  app.setGlobalPrefix(globalPrefix);
+	app.useGlobalInterceptors(
+		new ClassSerializerInterceptor(reflector),
+	);
+	app.useGlobalPipes(new ValidationPipe());
+
+  const swagger = initSwagger(app, Number(port), globalPrefix);
+  await app.listen(port, '0.0.0.0');
+
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
   );
+
+  Logger.log(swagger);
+
 }
 
 bootstrap();
