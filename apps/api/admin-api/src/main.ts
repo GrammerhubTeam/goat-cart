5150b338259481a838e5acc93102a59557243055
Alex Curtis-Slep <49458917+AlexVCS@users.noreply.github.com>
2022-07-28 07:33:49 -0400

    Gc 56 (#6)
    * utils library created
    
    * added dot env, configuring nest, and adding postgres configuration
    
    * added nest 9, working to resolve merge conflict
    
    * updating dependencies

---
+++ apps/api/admin-api/src/main.ts
@@ -0,0 +1,25 @@
+/**
+ * This is not a production server yet!
+ * This is only a minimal backend to get started.
+ */
+
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { NestFactory } from '@nestjs/core';
+
+import { AppModule } from './app/app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  const port = configService.get('PORT');
+
+  const globalPrefix = 'api'
+
+  await app.listen(port);
+  Logger.log(
+    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+  );
+}
+
+bootstrap();
---
+++ feature.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+interface EnvironmentVariables {
+  PORT: number;
+  TIMEOUT: string;
+}
+
+@Module({
+    imports: [ConfigModule],
+  })
+export class FeatureModule {
+    constructor(private configService: ConfigService<EnvironmentVariables>) {
+    const port = this.configService.get('PORT', { infer: true });
+    const url = this.configService.get('URL', { infer: true });
+    }
+    const dbUser = this.configService.get<string>('DATABASE_USER')
+    const dbPassword = this.configService.get<string>('DATABASE_PASSWORD');
+}
