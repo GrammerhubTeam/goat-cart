558e1212c2feb2029e313e702fe96f70c0161df9
cantuc40 <cantuc40@gator.uhd.edu>
2022-09-16 17:16:26 -0500

    imported MailerModule and HandlebarsAdapter

--- apps/api/messaging/src/modules/email/email.module.ts
+++ apps/api/messaging/src/modules/email/email.module.ts
@@ -1,4 +1,45 @@
 import { Module } from '@nestjs/common';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+import { hostname } from 'os';
+import { prototype } from 'events';
 
-@Module({})
+
+@Module({
+    imports: /*[
+      MailerModule.forRoot({
+        transport: 'smtps://user@domain.com:pass@smtp.domain.com',
+        defaults: { from: '"nest-modules" <modules@nestjs.com>', },
+        template: {
+          dir: __dirname + '/templates',
+          adapter: new HandlebarsAdapter(),
+          options: {
+            strict: true,
+          },
+        },
+      }),
+    ]*/ [MailerModule.forRoot({
+        transport: {
+          host: process.env.HOST,
+          port: process.env.PORT,
+          ignoreTLS: true,
+          secure: false,
+          auth: {
+            user: process.env.MAILDEV_INCOMING_USER,
+            pass: process.env.MAILDEV_INCOMING_PASS,
+          },
+        },
+        defaults: {
+          from: '"No Reply" <no-reply@localhost>',
+        },
+        preview: true,
+        template: {
+          dir: process.cwd() + '/template/',
+          adapter: new HandlebarsAdapter(), // or new PugAdapter() or new EjsAdapter()
+          options: {
+            strict: true,
+          },
+        },
+      }),], 
+})
 export class EmailModule {}
