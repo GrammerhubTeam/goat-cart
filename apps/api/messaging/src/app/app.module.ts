3385445c4349604a8a79c8a3da524337af9cc6c0
cantuc40 <cantuc40@gator.uhd.edu>
2022-09-16 17:11:59 -0500

    Added EmailModule and ConfigModule. imported services and controllers for sms and email

--- apps/api/messaging/src/app/app.module.ts
+++ apps/api/messaging/src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { Module } from '@nestjs/common';
-
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SmsModule } from '../modules/sms/sms.module';
 import { EmailModule } from '../modules/email/email.module';
+import { EmailService } from '../modules/email/email.service';
+import { SMSService } from '../modules/sms/sms.service';
+import { EmailController } from '../modules/email/email.controller';
+import { SMSController } from '../modules/sms/sms.controller';
+import configuration from '../configs/configuration'
 
 @Module({
   imports: [
     SmsModule,
-    EmailModule
+    EmailModule,
+    ConfigModule.forRoot({
+      //envFilePath: '.development.env',
+      load: [configuration],
+    }),
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, EmailController, SMSController],
+  providers: [AppService, EmailService, SMSService],
 })
 export class AppModule {}
